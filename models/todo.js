58022f849294779e834d087e7516e9f5d73621ef
Charlotte Maanianu Seyab <charlotteseyab19@gmail.com>
2024-10-17 09:54:14 +0000

    added user authentication and made changes

--- models/todo.js
+++ models/todo.js
@@ -1,9 +1,14 @@
 import { Schema, model } from "mongoose";
+import { toJSON } from "@reis/mongoose-to-json";
 
 const todoSchema = new Schema({
-    title: {type: String, required: true},
-    icon: {type: String, required: true},
-    completed: {type: Boolean, default: false}
+    title: { type: String, required: true },
+    icon: { type: String, required: true },
+    completed: { type: Boolean, default: false }
+}, {
+    timestamps: true
 });
+
+todoSchema.plugin(toJSON);
 
 export const TodoModel = model('Todo', todoSchema);
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, updateProfile } from "../controllers/user.js";
+import { registerUser, loginUser, logoutUser, updateProfile, getProfile } from "../controllers/user.js";
 import { userAvatarUpload } from "../middlewares/upload.js";
 
 //create router
@@ -10,6 +10,8 @@ const userRouter = Router();
 userRouter.post('/users/registerUser', registerUser);
 
 userRouter.post('/users/loginUser', loginUser);
+
+userRouter.get('/users/me', getProfile);
 
 userRouter.post('/users/logoutUser', logoutUser);
 
